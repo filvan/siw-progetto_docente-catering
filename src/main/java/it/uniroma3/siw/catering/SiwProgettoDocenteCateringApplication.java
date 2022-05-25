e5c7d7ab527658e39493238ffee9b1bd4d824fc4
filvan <fil.vannutelli@stud.uniroma3.it>
2022-05-25 21:43:41 +0200

    Primo commit

---
+++ src/main/java/it/uniroma3/siw/catering/SiwProgettoDocenteCateringApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.catering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwProgettoDocenteCateringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwProgettoDocenteCateringApplication.class, args);
+	}
+
+}
