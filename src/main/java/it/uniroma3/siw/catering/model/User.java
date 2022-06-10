1175fb6c2f1fa22c00a8998b05cc2a84cdaea470
filvan <fil.vannutelli@stud.uniroma3.it>
2022-06-10 16:34:27 +0200

    Modifiche per creare l'utente admin e il db dedicato al progetto su postgres

--- src/main/java/it/uniroma3/siw/catering/model/User.java
+++ src/main/java/it/uniroma3/siw/catering/model/User.java
@@ -5,6 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "users") // cambiamo nome perchè in postgres user e' una parola riservata
@@ -13,7 +14,11 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+	
+	@NotBlank
 	private String nome;
+	
+	@NotBlank
 	private String cognome;
 	
 	public Long getId() {
