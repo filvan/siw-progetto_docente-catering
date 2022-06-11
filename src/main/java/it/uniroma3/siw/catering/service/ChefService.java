dbb2a8bbc7f80152a7f0992cc4ad2832d23c86d7
filvan <fil.vannutelli@stud.uniroma3.it>
2022-06-11 12:18:03 +0200

    Create ChefService.java

---
+++ src/main/java/it/uniroma3/siw/catering/service/ChefService.java
@@ -0,0 +1,58 @@
+package it.uniroma3.siw.catering.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import it.uniroma3.siw.catering.model.Chef;
+import it.uniroma3.siw.catering.repository.ChefRepository;
+
+@Service
+public class ChefService {
+
+	
+	@Autowired
+	private ChefRepository chefRepository; 
+	
+	@Transactional
+	public Chef save(Chef chef) {
+		return chefRepository.save(chef);
+	}
+
+	@Transactional
+	public List<Chef> findAll() {
+		return (List<Chef>) chefRepository.findAll();
+	}
+
+	@Transactional
+	public Chef findById(Long id) {
+		Optional<Chef> optional = chefRepository.findById(id);
+		if (optional.isPresent())
+			return optional.get();
+		else 
+			return null;
+	}
+
+	@Transactional
+	public boolean alreadyExists(Chef chef) {
+		List<Chef> chefs = this.chefRepository.findByNomeAndCognome(chef.getNome(), chef.getCognome());
+		if (chefs.size() > 0)
+			return true;
+		else 
+			return false;
+	}
+	
+	@Transactional
+	public void deleteById(Long id) {
+		chefRepository.deleteById(id);
+	}
+	
+	@Transactional
+	public void delete(Chef chef) {
+		chefRepository.delete(chef);
+	}
+}
