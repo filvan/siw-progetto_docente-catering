2775046f6663c498db4794cb3755dd2f1b5af23a
filvan <fil.vannutelli@stud.uniroma3.it>
2022-06-12 21:46:42 +0200

    Update Buffet.java
    Cambio dell'associazione con i Piatti, da OneToMany a ManyToMany

--- src/main/java/it/uniroma3/siw/catering/model/Buffet.java
+++ src/main/java/it/uniroma3/siw/catering/model/Buffet.java
@@ -2,14 +2,15 @@ package it.uniroma3.siw.catering.model;
 
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 
 @Entity
@@ -28,8 +29,7 @@ public class Buffet {
 	@ManyToOne
 	private Chef chef;
 	
-	@OneToMany
-	@JoinColumn(name = "buffet_id")
+	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
 	private List<Piatto> piatti;
 
 	public Long getId() {
